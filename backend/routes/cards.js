55acfd461cc8fc2d98ddfb2c2dd75d81ef2c38ea
Mxnsoon <nigga.guy@yandex.ru>
2021-03-30 21:55:16 +0300

    fix

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -15,7 +15,7 @@ router.post('/cards', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     link: Joi.string().pattern(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*)/),
-  }).unknown(true),
+  }),
 }), postCard);
 
 router.delete('/cards/:cardId', celebrate({
