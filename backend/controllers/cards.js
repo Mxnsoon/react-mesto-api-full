40f76f4d75a36b2f4b2a85b2bcf69983b9db6c66
Mxnsoon <nigga.guy@yandex.ru>
2021-03-30 22:21:08 +0300

    fix

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -13,10 +13,10 @@ const postCard = (req, res, next) => {
   const { name, link } = req.body;
   const owner = req.user._id;
   Card.create({ name, link, owner })
-    .then((card) => res.send(card))
+    .then((card) => res.status(200).send(card))
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        throw new BadRequestError('Данные введены неверно');
+        next(new BadRequestError('Данные введены неверно'));
       }
       next(err);
     });
